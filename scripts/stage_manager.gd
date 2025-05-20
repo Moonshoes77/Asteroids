f430dae13a491fa01f885292a0b6f6e63e765f86
Moonshoes77 <moonshoespotter77@gmail.com>
2025-05-19 22:41:15 -0400

    restored listener

--- scripts/stage_manager.gd
+++ scripts/stage_manager.gd
@@ -5,24 +5,31 @@ var lives: int
 var score: int = 0
 
 enum Difficulty {EASY, NORMAL, HARD} 
-var input = Array([], TYPE_STRING,"",null)
+var input = Array(["", "", "", "", "", "", ""], TYPE_STRING,"",null)
+const MAX_INPUT_LENGTH: int = 6
 
 
 func _input(event: InputEvent) -> void:
-	if event is InputEventKey:
-		input.push_back(OS.get_keycode_string(event.keycode))
-		print(input)
+	if event is InputEventKey && !event.is_pressed():
+		store_input(event)
 	#else:
 		#pass
 
 
-func store_input():
-	pass
+func store_input(event):
+	input.pop_front()
+	input.append(OS.get_keycode_string(event.keycode))
+	print(input)
+	check_input(event)
 
+func check_input(event):
+	if "".join(input).to_lower() == "pissboy":
+		print("Pissboy activated!")
 
 func _ready() -> void:
-	print(Difficulty)
+	#print(Difficulty)
+	print(input)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	store_input()
+	pass
